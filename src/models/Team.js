ac56e93c5d89752cf8178d6678d065baf4021fac
duyngo0605 <duydaik03@gmail.com>
2025-01-25 14:06:09 +0700

    fix team model bug

--- src/models/Team.js
+++ src/models/Team.js
@@ -2,18 +2,18 @@
 const mongoose = require('mongoose');
 
 const TeamSchema = new mongoose.Schema({
-    idHost: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    idHost: { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
     name: { type: String, required: true, unique: true },
     image: { type: String, required: true},
     maxParticipant: { type: Number, required: true },
     members: [{
-        member: { Type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+        member: { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
         role: {
             Type: String,
             enum: ['manager', 'participant']
         }
     }],
-    quizs: [{Type: mongoose.Schema.Types.ObjectId, ref: 'Quiz'}]
+    quizs: [{type: mongoose.Schema.Types.ObjectId, ref: 'Quiz'}]
 }, { timestamps: true });
 
 module.exports = mongoose.model('Team', TeamSchema);
---
+++ src/services/TeamService.js
@@ -0,0 +1,113 @@
+const Team = require('../models/Team')
+
+const createTeam = async (newTeam) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const createdTeam = await Team.create(newTeam)
+            if (createdTeam)
+            { 
+            resolve({
+                status: 'OK',
+                message: 'SUCCESS',
+                data: createdTeam
+            })
+            }
+        }
+
+        catch (e) {
+            reject(e)
+        }
+    })
+}
+
+
+const getTeam = (id) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!id) {
+                const allTeam = await Team.find()
+                resolve({
+                    status: 'OK',
+                    message: 'Success',
+                    data: allTeam
+                })
+            }
+            else {
+                const Team = await Team.findOne({
+                    _id: id
+                })
+                if (Team === null) {
+                    resolve({
+                        status: 'ERR',
+                        message: 'The Team is not defined'
+                    })
+                }
+                resolve({
+                    status: 'OK',
+                    message: 'SUCCESS',
+                    data: Team
+                })
+            }
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+const updateTeam = async (TeamId, data) => {
+    return new Promise(async (resolve, reject) => {
+
+        try {
+            const checkTeam = await Team.findOne({
+                _id: TeamId
+            })
+            if (!checkTeam){
+                resolve({
+                    status: 'ERR',
+                    message: 'The Team is not defined.'
+                })
+            }
+
+            const updatedTeam = await Team.findByIdAndUpdate(TeamId, data, {new: true})
+            resolve({
+                status: 'OK',
+                message: 'SUCCESS',
+                data: updatedTeam
+            })
+        }
+
+        catch (e) {
+            reject(e)
+        }
+    })
+}
+
+const deleteTeam = (id) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const checkTeam = await Team.findOne({
+                _id: id
+            })
+            if (checkTeam === null) {
+                resolve({
+                    status: 'ERR',
+                    message: 'The Team is not defined'
+                })
+            }
+            await Team.findByIdAndDelete(id)
+            resolve({
+                status: 'OK',
+                message: 'Delete Team success',
+            })
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+module.exports = {
+    createTeam,
+    getTeam,
+    updateTeam,
+    deleteTeam,
+}
